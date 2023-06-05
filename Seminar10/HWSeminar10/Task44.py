e830ce4f5045660f90a56dca0024ffa5ff91936e
Mike Misik <mike-misik@yandex.ru>
2023-06-05 21:20:11 +0300

    Task44 realized

---
+++ Seminar10/HWSeminar10/Task44.py
@@ -0,0 +1,25 @@
+# Задача 44: 
+# В ячейке ниже представлен код, генерирующий DataFrame, 
+# которая состоит всего из 1 столбца. 
+# Ваша задача перевести его в one hot вид. 
+# Сможете ли вы это сделать без get_dummies?
+
+
+
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+print(data.head())
+print()
+
+data = data.replace('robot', '1')
+# print(data.head())
+# print()
+
+data = data.replace('human', '0')
+print(data.head())
+print()
